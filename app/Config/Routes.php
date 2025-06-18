54efb1044bca5770356a827bd8701b2054689558
ferdynand <ferdynandergypramudani11@gmail.com>
2025-06-18 16:08:03 +0700

    menambahkan api data transaksi pembelian

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -34,6 +34,7 @@ $routes->post('buy', 'TransaksiController::buy', ['filter' => 'auth']);
 $routes->get('get-location', 'TransaksiController::getLocation', ['filter' => 'auth']);
 $routes->get('get-cost', 'TransaksiController::getCost', ['filter' => 'auth']);
 
+$routes->get('profile', 'Home::profile', ['filter' => 'auth']);
 $routes->get('/keranjang', 'TransaksiController::index', ['filter' => 'auth']);
 $routes->get('/faq' ,'Home::faq',['filter' => 'auth']);
 
@@ -42,4 +43,4 @@ $routes->post('produk_category/create', 'ProdukCategoryController::create');
 $routes->post('produk_category/edit/(:num)', 'ProdukCategoryController::edit/$1');
 $routes->get('produk_category/delete/(:num)', 'ProdukCategoryController::delete/$1');
 
-
+$routes->resource('api', ['controller' => 'apiController']);
--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -29,7 +29,16 @@
                 <i class="bi bi-receipt"></i>
                 <span>Produk</span>
             </a>
-        </li><!-- End Produk Nav -->         <li class="nav-item">
+        </li><!-- End Produk Nav -->         
+
+        <li class="nav-item">
+            <a class="nav-link <?php echo (uri_string() == 'profile') ? "" : "collapsed" ?>" href="profile">
+            <i class="bi bi-person"></i>
+            <span>Profile</span>
+        </a>
+        </li><!-- End Profile Nav -->
+        
+        <li class="nav-item">
             <a class="nav-link <?php echo (uri_string() == 'faq') ? "" : "collapsed" ?>" href="faq">
                 <i class="bi bi-info"></i>
                 <span>Faq</span>
