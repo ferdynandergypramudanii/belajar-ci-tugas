3130beb8295e3b9fe035ae173956f9e259ba3de6
ferdynand <ferdynandergypramudani11@gmail.com>
2025-07-05 00:28:55 +0700

    some changes to the seeder

--- app/Database/Seeds/UserSeeder.php
+++ app/Database/Seeds/UserSeeder.php
@@ -4,22 +4,37 @@ namespace App\Database\Seeds;
 
 use CodeIgniter\Database\Seeder;
 
-class UserSeeder extends Seeder
+class DiskonSeeder extends Seeder
 {
     public function run()
     {
-        $faker = \Faker\Factory::create('id_ID');
+        // Daftar nominal diskon sesuai permintaan
+        $nominalDiskon = [
+            100000,
+            120000,
+            105000,
+            200000,
+            150000,
+            145000,
+            50000,
+            75000,
+            110000,
+            300000
+        ];
+
+        $startDate = strtotime('2025-07-04');
 
         for ($i = 0; $i < 10; $i++) {
+            $tanggal = date('Y-m-d', strtotime("+$i day", $startDate));
+
             $data = [
-                'username' => $faker->userName,
-                'email' => $faker->email,
-                'password' => password_hash('1234567', PASSWORD_DEFAULT),
-                'role' => $faker->randomElement(['admin', 'guest']),
-                'created_at' => date("Y-m-d H:i:s"),
+                'tanggal'    => $tanggal,
+                'nominal'    => $nominalDiskon[$i],
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
             ];
-            //print_r($data);
-            $this->db->table('user')->insert($data);
+
+            $this->db->table('diskon')->insert($data);
         }
     }
 }
